f4592170ec1df84f6747939d2b9b71dc20b2d727
rosiegrant <rosellen.grant@gmail.com>
2021-01-06 11:02:25 -0500

    a little prettier now

--- src/ProductCard.tsx
+++ src/ProductCard.tsx
@@ -16,23 +16,25 @@ const ProductCard = ({ product, showQuickLook, addToCart }: Props) => {
   return (
     <div
       className={classnames(
-        "rounded cursor-pointer transform  transition ease-in-out group overflow-hidden relative",
+        "rounded cursor-pointer m-2 transform transition ease-in-out group overflow-hidden relative",
         "md:hover:shadow-xl md:hover:bg-white md:hover:z-30 md:hover:scale-105 md:hover:z-100"
       )}
     >
-      <div className="p-4" onClick={showQuickLook}>
-        <div className=" aspect-w-4 aspect-h-2">
+      <div onClick={showQuickLook}>
+        <div className="aspect-w-1 aspect-h-1">
           {photoGallery.length > 0 && (
-            <div className="flex items-center  p-4">
+            <div className="flex items-center">
               <img src={photoGallery[0].image.url} alt="" width="100%" />
             </div>
           )}
         </div>
-        <div className="font-medium text-black mt-4">{name}</div>
-        {/* <div className="text-gray-500 font-light text-sm">
-        </div> */}
-        <div className="text-gray-500 font-light text-sm">2 colors</div>
-        <div className=" font-medium mt-2 text-sm">${price.value}</div>
+        <div className="p-2">
+          <div className="font-medium text-black mt-4">{name}</div>
+          {/* <div className="text-gray-500 font-light text-sm">
+          </div> */}
+          <div className="text-gray-500 font-light text-sm">2 colors</div>
+          <div className=" font-medium mt-2 text-sm">${price.value}</div>
+        </div>
       </div>
 
       <div className="flex opacity-0 md:group-hover:opacity-100 text-gray-500 uppercase text-xs transition ease-in-out  justify-center">
